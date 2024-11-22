610e72a3bae02cf572b518af0f2a335ea4df2082
Upendra Kumar <toxic74412@gmail.com>
2024-11-22 20:50:15 +0530

    setup express app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log("Hello main!");
+import express, { Request, Response } from "express";
+import cors from "cors";
+import http from "http";
+
+const app = express();
+const PORT = process.env.PORT || 8001;
+
+app.use(cors({ credentials: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res
+    .status(200)
+    .json({ status: "success", message: "Hello World from Express!" });
+});
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
